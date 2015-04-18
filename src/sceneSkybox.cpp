1dd7cf897325e24472747cb1991fbc7b3a244edd
Joshua Zhang <woncomp@gmail.com>
2015-04-18 15:56:35 +0800

    Add sceneSkybox code files.

---
+++ src/sceneSkybox.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ck01- on 2015/4/18.
+//
+
+#include "sceneSkybox.h"
---
+++ src/sceneSkybox.h
@@ -0,0 +1,14 @@
+//
+// Created by ck01- on 2015/4/18.
+//
+
+#ifndef GLHEART_SCENESKYBOX_H
+#define GLHEART_SCENESKYBOX_H
+
+
+class sceneSkybox {
+
+};
+
+
+#endif //GLHEART_SCENESKYBOX_H
